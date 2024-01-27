b5b922be4246b0818ceb922167dba90e04d6c4b1
Zach MacLean-Guy <zc489813@dal.ca>
2024-01-27 12:38:34 -0400

    made folder structure

--- Scripts/CombatScripts/Bullet.gd
+++ Scripts/CombatScripts/Bullet.gd
@@ -1,16 +1,18 @@
 extends Area2D
+class_name Bullet
 
 @export var damage = 1;
 var screenSize;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(name + " ready");
 	screenSize = get_viewport_rect().size;
 	get_tree().root.connect("size_changed", _on_viewport_size_changed);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	doMovementPhase();
 
 func _on_viewport_size_changed():
 	var generalPosX = position.x - 0.5 * screenSize[0];
@@ -19,3 +21,6 @@ func _on_viewport_size_changed():
 	screenSize = get_viewport_rect().size;
 	position.x = screenSize[0]*0.5 + generalPosX;
 	position.y = screenSize[1]*0.5 + generalPosY;
+
+func doMovementPhase():
+	pass;
