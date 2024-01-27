5b13bb0fcbdc61d5420fe617d7645baa39e8f731
Zach MacLean-Guy <zc489813@dal.ca>
2024-01-26 20:50:21 -0400

    something malicious is brewing >:))))

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@export var speed = 100;
+var screenSize;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screenSize = get_viewport_rect().size;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	##I'm saying for now that moving at an angle can just be faster than moving straight and that's fine
+	var movingDown = 0;
+	if (Input.is_action_pressed("move up")):
+		movingDown -= 1;
+	
+	if (Input.is_action_pressed("move down")):
+		movingDown += 1;
+		
+	var movingRight = 0;
+	if (Input.is_action_pressed("move right")):
+		movingRight += 1;
+	
+	if (Input.is_action_pressed("move left")):
+		movingRight -= 1;
+	
+	var velocity = Vector2(movingRight*speed, movingDown*speed);
+	
+	position += velocity * delta;
+	
