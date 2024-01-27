bb4cb150779c39f7d1635fee65b5634a0a073e88
= <sh843173@dal.ca>
2024-01-27 16:55:34 -0400

    Add healthbar and take-damage event listener

---
+++ Scripts/UIScripts/Healthbar.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+
+var health = 3
+var	healthBarImgs = [
+	null,
+	load("res://Sprites/1hp.png"),
+	load("res://Sprites/2hp.png"),
+	load("res://Sprites/3hp.png")
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func on_take_damage():
+	health -= 1
+	texture = healthBarImgs[health]
