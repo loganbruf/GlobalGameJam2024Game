7b2889d8cb795d83d31282eb091daad9a7941fc0
Zach MacLean-Guy <zc489813@dal.ca>
2024-01-27 20:45:24 -0400

    fixed things rotating weird

--- Scripts/CombatScripts/PlayerBullet.gd
+++ Scripts/CombatScripts/PlayerBullet.gd
@@ -7,12 +7,13 @@ func _ready():
 	super._ready();
 	damage = 0;
 	enemyDamage = 1;
+	damage = 0;
 	speed  = 500;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func doMovementPhase(delta):
 	position += delta * direction * speed;
-	var bulletAngle = atan2(direction[0], direction[1]);
+	var bulletAngle = -atan2(direction[0], direction[1]);
 	
 	rotation = bulletAngle;
 
