41cff7d7d260b5cecb1d9a28701943ba81d44c9e
Anas Alhadi <Alhadi@dal.ca>
2024-01-28 13:44:29 -0400

    link

--- Scripts/CutsceneScripts/CutsceneManager.gd
+++ Scripts/CutsceneScripts/CutsceneManager.gd
@@ -5,12 +5,15 @@ extends Node2D
 @export var next_scene: String
 var is_dialogue_open : bool = false
 
+
 func _ready():
 	$EnterKey.hide()
 
 func _process(delta):
-	if Input.is_action_just_pressed("next"):
+	if !is_dialogue_open && Input.is_action_just_pressed("next"):
 		SceneVariables.goto_scene(next_scene)
+	elif Input.is_action_just_pressed("next"):
+		OS.shell_open("https://media1.tenor.com/m/qldk8W8R2WIAAAAd/jikowrld-druski.gif")
 
 func _input(event):
 	if event is InputEventKey && !is_dialogue_open:
